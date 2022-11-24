022ed91f607331c0b3315e6d1e9ef6f66be23df6
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2022-11-23 22:36:07 -0300

    all up and running in .py file, needs metrics for SNR and intel

--- TP2_DSP_Haimovich_Veiga.py
+++ TP2_DSP_Haimovich_Veiga.py
@@ -45,6 +45,32 @@ plt.title("Vega con ruido")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 
+# Detección de secciones con ruido.
+def onlyNoiseRangeDetection(x):
+    energia1 = librosa.feature.rms(y=signal1,frame_length=2400,hop_length=100)
+    avgSTE1 = np.mean(energia1)
+    signal1_VA = np.zeros(len(energia1[0]))
+    for i in range(0,len(energia1[0])):
+        if energia1[0][i] < (avgSTE1):
+            signal1_VA[i]=1
+        else:
+            signal1_VA[i]=0
+
+    noiseRanges = [[0,0]]
+    nOfRanges = 0
+    longestRange = 0
+    longestRange_len = 0
+    for i in range(0,len(signal1_VA)):
+        if signal1_VA[i]==1:
+            noiseRanges[nOfRanges][1] = i 
+        else:
+            if (noiseRanges[nOfRanges][1] - noiseRanges[nOfRanges][0]) > longestRange_len:
+                longestRange = nOfRanges
+                longestRange_len = noiseRanges[nOfRanges][1] - noiseRanges[nOfRanges][0]
+            nOfRanges += 1
+            noiseRanges.append([i,i])
+    return x[noiseRanges[longestRange][0]:noiseRanges[longestRange][1]]
+
 def spectralSubtraction(x,M,hop):
     # STFT de x
     Y = librosa.stft(x)
@@ -52,7 +78,7 @@ def spectralSubtraction(x,M,hop):
     Y_abs = np.abs(Y)
     Y_phase = np.angle(Y)
     # Selección de rango de la señal que contenga sólo ruido
-    noise=np.array(x[0:int(5*fs)])
+    noise=onlyNoiseRangeDetection(x)
     # Promedio espectral del ruido (mu)
     mu=np.mean(abs(fft(noise)))
     # Estimador de la magnitud de la señal original
@@ -77,27 +103,27 @@ plt.figure(2, figsize=(25,15))
 plt.subplot(3,1,1)
 plt.grid()
 plt.plot(t1[:len(signal1_boll)], signal1_boll)
-plt.title("Clarinete sin ruido")
+plt.title("Clarinete sin ruido (Boll)")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 
 plt.subplot(3,1,2)
 plt.grid()
 plt.plot(t1[:len(signal2_boll)], signal2_boll)
-plt.title("Glock sin ruido")
+plt.title("Glock sin ruido (Boll)")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 
 plt.subplot(3,1,3)
 plt.grid()
 plt.plot(t1[:len(signal3_boll)], signal3_boll)
-plt.title("Vega sin ruido")
+plt.title("Vega sin ruido (Boll)")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 
-sf.write('signal1_boll.wav',signal1_boll,fs)
-sf.write('signal2_boll.wav',signal2_boll,fs)
-sf.write('signal3_boll.wav',signal3_boll,fs)
+sf.write('Clarinet_boll.wav',signal1_boll,fs)
+sf.write('Glock_boll.wav',signal2_boll,fs)
+sf.write('Vega_boll.wav',signal3_boll,fs)
 
 #%%
 
@@ -125,9 +151,9 @@ signal3 = signal3[:,0]
 t1=np.linspace(0, len(signal1)//fs,len(signal1))
 
 def SNR(x,mu):
-    x_p = np.mean(x)**2
+    x_p = np.mean(abs(x))**2
     mu_p = mu**2
-    SNR = (x_p)/mu_p
+    SNR = ((x_p)/mu_p)
     return SNR
 
 def alpha_i(x,mu):
@@ -155,25 +181,31 @@ def MBSS(x,M,hop,bands):
     Y_abs = np.abs(Y)
     Y_phase = np.angle(Y)
     # Selección de rango de la señal que contenga sólo ruido
-    noise=np.array(x[0:int(5*fs)])
-    # Promedio espectral del ruido (mu)
-    mu=np.mean(abs(fft(noise)))
+    noise=onlyNoiseRangeDetection(x)
     # Defino el parámetro de piso espectral Beta
     beta = 0.02
     # Estimador de la magnitud de la señal original calculado por bandas
-    S_squared = np.zeros_like(Y)
-    for i in range(bands):
-        for j in range(len(Y[0])):
-            for k in range(i*(len(Y)//bands),(i+1)*(len(Y)//bands)):
-                Y_p = Y[k][j]**2
-                alpha = alpha_i(Y[i*(len(Y)//bands):(i+1)*(len(Y)//bands),j], mu)
-                delta = delta_i((fs/len(Y))*(i+1)*(len(Y)//bands), fs)
-                S_squared[k][j] = Y_p - (alpha*delta*(mu**2))
-                if (S_squared[k][j]) < (beta*Y_p):
-                    S_squared[k][j]=beta*(Y_p)
+    Y_t = Y.transpose()
+    S_squared_t = np.zeros_like(Y_t)
+    for i in range(len(Y_t)):
+        for k in range(bands):
+            band_start = k*(len(Y_t[i])//(bands+1))
+            band_end = (k+2)*(len(Y_t[i])//(bands+1))
+            band_i = Y_t[i][band_start:band_end]
+            band_i = band_i*np.hanning(len(band_i))
+            # Promedio espectral del ruido de la banda (mu)
+            mu_i=np.mean(abs(fft(noise[band_start:band_end])))
+            for j in range(len(band_i)):
+                Y_p = band_i[j]**2
+                alpha = alpha_i(band_i, mu_i)
+                delta = delta_i(band_end, fs)
+                S_squared_t[i][j+(k*len(band_i)//2)] += Y_p - (alpha*delta*(mu_i**2))
+                if (S_squared_t[i][j+(k*len(band_i)//2)]) < (beta*Y_p):
+                    S_squared_t[i][j+(k*len(band_i)//2)] += beta*(Y_p)
+    S_squared = S_squared_t.transpose()
     # Incorporación de fase y transformada inversa
     S_f = np.sqrt(S_squared)*np.exp(1j*Y_phase)
-    noiseless_s = librosa.istft(S_f)
+    noiseless_s = librosa.istft(Y_t.transpose())
     return noiseless_s
 
 signal1_MBSS = MBSS(signal1,1536,768,4)
@@ -186,71 +218,24 @@ plt.figure(2, figsize=(25,15))
 plt.subplot(3,1,1)
 plt.grid()
 plt.plot(t1[:len(signal1_MBSS)], signal1_MBSS)
-plt.title("Clarinete sin ruido")
+plt.title("Clarinete sin ruido (MBSS)")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 
 plt.subplot(3,1,2)
 plt.grid()
 plt.plot(t1[:len(signal2_MBSS)], signal2_MBSS)
-plt.title("Glock sin ruido")
+plt.title("Glock sin ruido (MBSS)")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 
 plt.subplot(3,1,3)
 plt.grid()
 plt.plot(t1[:len(signal3_MBSS)], signal3_MBSS)
-plt.title("Vega sin ruido")
+plt.title("Vega sin ruido (MBSS)")
 plt.xlabel("Tiempo")
 plt.ylabel("Amplitud")
 
-sf.write('signal1_MBSS.wav',signal1_MBSS,fs)
-sf.write('signal2_MBSS.wav',signal2_MBSS,fs)
-sf.write('signal3_MBSS.wav',signal3_MBSS,fs)
-
-
-#%%
-
-# Detección de secciones con ruido.
-
-energia1 = librosa.feature.rms(signal1,2400,100)
-avgSTE1 = np.mean(energia1)
-signal1_VA = np.zeros(len(energia1[0]))
-for i in range(0,len(energia1[0])):
-    if energia1[0][i] < (avgSTE1):
-        signal1_VA[i]=1
-    else:
-        signal1_VA[i]=0
-
-noiseRanges = [[0,0]]
-nOfRanges = 0
-longestRange = 0
-for i in range(0,len(signal1_VA)):
-    if signal1_VA[i]==1:
-       noiseRanges[nOfRanges][1] = i 
-    else:
-        if (noiseRanges[nOfRanges][1] - noiseRanges[nOfRanges][0]) > longestRange:
-            longestRange = nOfRanges
-        nOfRanges += 1
-        noiseRanges.append([i,i])
-
-print(noiseRanges)
-
-print(longestRange)
-print(noiseRanges[longestRange])
-
-plt.figure(3, figsize=(25,15))
-plt.grid()
-plt.plot(np.linspace(0,len(signal1)//fs,len(energia1[0])), energia1[0])
-plt.plot(np.linspace(0,len(signal1)//fs,len(energia1[0])), signal1_VA)
-
-zeros1 = librosa.feature.zero_crossing_rate(signal1,500,100)
-
-signal1_noise = []
-
-print(zeros1)
-plt.figure(4, figsize=(25,15))
-plt.grid()
-plt.plot(np.linspace(0,len(signal1)//fs,len(zeros1[0])), zeros1[0])
-
-# %%
+sf.write('Clarinet_MBSS.wav',signal1_MBSS,fs)
+sf.write('Glock_MBSS.wav',signal2_MBSS,fs)
+sf.write('Vega_MBSS.wav',signal3_MBSS,fs)
